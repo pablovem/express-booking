2eb43679d894dffdf3386080a45f3bb3355d738b
Pablovem <pablovema@gmail.com>
2021-07-24 15:59:01 -0500

    feat: add initial express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Server Setup
+const PORT = 3000;
+
+// Express Setup
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening on ${PORT}`);
+});
+
