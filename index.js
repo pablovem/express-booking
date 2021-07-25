709a106dfbfc28e61e139b12d93a343597ee2908
Pablovem <pablovema@gmail.com>
2021-07-25 00:22:57 -0500

    feat: setup lowdb
    update index.js and setup lowdb

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const lowdDB = require('lowdb');
+const FyleSync = require('lowdb/adapters/FileSync');
 
 // Server Setup
 const PORT = 3000;
+
+// Database Setup
+const adapter = new FyleSync('db.json');
+const db = lowdDB(adapter);
 
 // Express Setup
 const app = express();
