686d59e71e4a978a38460fc4482df642205d25bb
Pablovem <pablovema@gmail.com>
2021-07-25 01:05:20 -0500

    feat: update index.js
    add endpoint to fetch all users

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const lowdDB = require('lowdb');
 const FyleSync = require('lowdb/adapters/FileSync');
 
 // Server Setup
-const PORT = 3000;
+const PORT = 4000;
 
 // Database Setup
 const adapter = new FyleSync('db.json');
 const db = lowdDB(adapter);
+db.defaults({ users: [] }).write();
 
 // Express Setup
 const app = express();
+
+// Users
+// fetch users
+app.get('/api/users', (req, res) => {
+  const users = db.get('users').value(); // query
+  res.status(200).json({ success: true, data: users });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
