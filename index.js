f5a148342d111b5d29cf2d1e7a4c4947326a8ff7
Pablovem <pablovema@gmail.com>
2021-07-28 20:23:14 -0500

    feat: update index.js
    add endpoint to get an user by id

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ db.defaults({ users: [] }).write();
 const app = express();
 
 // Users
-// fetch users
+// fetch all users
 app.get('/api/users', (req, res) => {
   const users = db.get('users').value(); // query
   res.status(200).json({ success: true, data: users });
+});
+
+// get an user by id
+app.get('/api/users/:id', (req, res) => {
+  const { id } = req.params
+  const user = db.get('users').find({ _id: id }).value(); // query
+  res.status(200).json({ success: true, data: user });
 });
 
 app.listen(PORT, () => {
